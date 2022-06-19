5633e407d1cf94899e883e891287b3713f530e08
Filmon <filmon@Lydias-Air.attlocal.net>
2022-06-19 05:24:42 -0400

    Initialized Server.

---
+++ index.js
@@ -0,0 +1,33 @@
+//ENTRY POINT OF SERVER
+//when server is spinned, this file is executed. 
+require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth";
+
+const morgan = require("morgan");
+
+//server running with express to access functionalities. All of the value is stored in the variable app.
+
+const app = express();
+const http = require("http").createServer(app);
+
+
+// db connection
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB CONNECTION ERROR: ", err));
+
+// middlewares
+app.use(express.json({ limit: "4mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+
+// route middlewares
+//prefix with /api/ 
+app.use("/api", authRoutes);
+
+app.listen(8000, () => console.log("Server running on port 8000"));
