d147500130161400df32ec7dceb677505b21d538
Filmon <filmon@Lydias-Air.attlocal.net>
2022-06-19 18:09:39 -0400

    connecting  with mongo  and sendgrid api for email feature.

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import authRoutes from "./routes/auth";
 const morgan = require("morgan");
 
 //server running with express to access functionalities. All of the value is stored in the variable app.
-
 const app = express();
 const http = require("http").createServer(app);
 
@@ -21,8 +20,10 @@ mongoose
   .catch((err) => console.log("DB CONNECTION ERROR: ", err));
 
 // middlewares
+// data will be received as JSON data, maximum of 4 megabytes.
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true }));
+// apply cors so there is no conflict between different client running in another domain.
 app.use(cors());
 app.use(morgan("dev"));
 
