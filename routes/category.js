a645d3c9602bc6e2024156db7634111ee9a5aab7
Filmon <filmon@Lydias-Air.attlocal.net>
2022-06-20 19:25:16 -0400

    middlewares index file added.

--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
 
+// middleware
+import { requireSignin } from "../middlewares";
 // controllers
 import { create } from "../controllers/category";
 
-router.post("/category", create);
+router.post("/category", requireSignin, create);
 
 export default router;
